9279d517157d65db35f5c333f3f86c34ad8c108e
Nurhak Kaya <Nurhak.Kaya@felinesoft.com>
2020-04-10 16:38:54 +0100

    home template initial update

--- CleanBlog.Web/App_Data/Models/all.generated.cs
+++ CleanBlog.Web/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "8c6f7e68c388665e")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
