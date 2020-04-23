8b321ed3f11c98916316467b4401b23d38bc9841
Nurhak Kaya <Nurhak.Kaya@felinesoft.com>
2020-04-23 20:40:24 +0100

    articles basic pagination added

---
+++ CleanBlog.Core/Helpers/QueryStringHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+
+namespace CleanBlog.Core.Helpers
+{
+    public static class QueryStringHelper
+    {
+        public static int GetIntFromQueryString(HttpRequestBase request, string key, int fallbackValue = 0)
+        {
+            var stringValue = request.QueryString[key];
+            if (stringValue != null &&
+                !string.IsNullOrWhiteSpace(stringValue) &&
+                int.TryParse(stringValue, out var numbericValue))
+            {
+                return numbericValue;
+            }
+
+            return fallbackValue;
+        }
+    }
+}
