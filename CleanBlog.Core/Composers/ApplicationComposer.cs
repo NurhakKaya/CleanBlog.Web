aebf280329b5f62db5409226c8b06ad5ff08c880
Nurhak Kaya <Nurhak.Kaya@felinesoft.com>
2020-05-05 11:44:21 +0100

    slack intergration done

---
+++ CleanBlog.Core/Composers/ApplicationComposer.cs
@@ -0,0 +1,62 @@
+﻿using SlackBotMessages;
+using SlackBotMessages.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Configuration;
+using Umbraco.Core.Composing;
+
+namespace CleanBlog.Core.Composers
+{
+    public class ApplicationComposer : ComponentComposer<ApplicationComponent>, IUserComposer
+    {
+
+    }
+
+    public class ApplicationComponent : IComponent
+    {
+        public void Initialize()
+        {
+            try
+            {
+                // A simple example of a message which looks like it has been send by an alien
+                var client = new SbmClient(WebConfigurationManager.AppSettings["SlackBotMessagesWebHookUrl"]);
+
+                var message = new Message
+                {
+                    Username = "Alien",
+                    Text = "Hello from an Alien",
+                    IconEmoji = ":robot_face:",
+                    Attachments = new List<Attachment>()
+                {
+                    new Attachment()
+                    {
+                        Fallback="Clean Blog Website Started",
+                        Color="good",
+                        Fields=new List<Field>()
+                        {
+                            new Field()
+                            {
+                                Value="Clean Blog Website Started"
+                            }
+                        }
+                    }
+                }
+                    //IconEmoji = Emoji.Alien
+                };
+
+                client.Send(message);
+                // or send it fully async like this:
+                // await client.SendAsync(message).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Current.Logger.Error(typeof(ApplicationComponent), ex, "Unable to send slack message to notify site starting up.");
+            }
+        }
+
+        public void Terminate()
+        {
+
+        }
+    }
+}
